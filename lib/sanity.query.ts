f088f13b20b0d18762977675e31f485cd769ae5f
S E P E H R <sepehrhashemimamaquani1996@gmail.com>
2025-09-24 21:06:27 +0330

    🛺 fix(sanity): add slug field to author query
    - Added 'slug' property mapped from slug.current in authorQuery
    - Ensured author data includes slug for routing or identification purposes

--- lib/sanity.query.ts
+++ lib/sanity.query.ts
@@ -33,4 +33,4 @@ export const postField = groq`
 
 export const allPostsQuery = groq`*[_type == "post"]{ ${postField} } | order(date desc)`;
 
-export const authorQuery = groq`*[_type == "author"]{ name, 'image': {'url': avatar.asset->url, 'alt': name, 'lqip': avatar.asset->metadata.lqip }, xUrl }[0]`;
+export const authorQuery = groq`*[_type == "author"]{ name, 'image': {'url': avatar.asset->url, 'alt': name, 'lqip': avatar.asset->metadata.lqip }, xUrl, 'slug': slug.current }[0]`;
