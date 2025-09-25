7729cca1d0009eb1026a53f32bfa6ea234b8afd4
S E P E H R <sepehrhashemimamaquani1996@gmail.com>
2025-09-25 20:58:15 +0330

    🪒 feat(sharepost): add LinkedIn share button and update share URLs
    - Import FaLinkedin icon from react-icons/fa6
    - Add LinkedIn to socialMedia array with proper sharing URL
    - Update other socialMedia URLs to include dynamic text, title, and fullUrl
    - Replace native share API with popup window for sharing URLs
    - Implement popupWindow function to open share links in a new window
    - Remove unused handleShare function and related code
    - Add

--- components/Common/SharePost.tsx
+++ components/Common/SharePost.tsx
@@ -5,6 +5,7 @@ import { BsWhatsapp } from 'react-icons/bs';
 import { FaFacebookSquare } from 'react-icons/fa';
 import { LiaTelegram } from 'react-icons/lia';
 import { RiTwitterXFill } from 'react-icons/ri';
+import { FaLinkedin } from "react-icons/fa6";
 
 type SharePostProps = {
   title: string;
@@ -12,32 +13,34 @@ type SharePostProps = {
   text: string;
 };
 
+const SharePost = ({ text, url, title }: SharePostProps) => {
+  const fullUrl = typeof window !== 'undefined' ? window.location.href : url;
+
+  
 const socialMedia = [
-  { name: 'telegram', Icon: LiaTelegram },
-  { name: 'whatsapp', Icon: BsWhatsapp },
-  { name: 'twitter', Icon: RiTwitterXFill },
-  { name: 'facebook', Icon: FaFacebookSquare },
+  { name: 'telegram', Icon: LiaTelegram, url: `https://t.me/share/url?url=${fullUrl}&text=${text}` },
+  { name: 'whatsapp', Icon: BsWhatsapp, url: `https://api.whatsapp.com/send?text=${encodeURIComponent(
+        "پست جدید از وبلاگ"
+      )}.%0A%0A${title}%0A%0A${fullUrl}` },
+  { name: 'twitter', Icon: RiTwitterXFill, url: `https://twitter.com/intent/tweet?text=${encodeURIComponent(
+        "پست جدید از وبلاگ"
+      )}.%0A%0A${title}%0A%0A${fullUrl}` },
+  { name: 'facebook', Icon: FaFacebookSquare, url: `https://www.facebook.com/sharer.php?u=${fullUrl}` },
+  { name: 'linkedin', Icon: FaLinkedin, url: `https://linkedin.com/sharing/share-offsite/?url=${fullUrl}}&title=${title}&summary=${text}` },
 ];
 
-const SharePost = ({ text, url, title }: SharePostProps) => {
-  const handleShare = async () => {
-    if (navigator.share) {
-      try {
-        await navigator.share({ title, url, text });
-      } catch (error) {
-        console.error('خطا در اشتراک گذاری: ', error);
-      }
-    } else {
-      alert('مرورگر شما از اشتراک گذاری پشتیبانی نمی کند.');
-    }
-  };
+const popupWindow = (url: string) => {
+  window.open(url, '', 'width=600,height=400,left=200,top=200');
+};
+
   return (
     <div className="w-full h-full py-6 px-1 flex flex-row gap-2 flex-wrap justify-center items-center">
       {socialMedia.map((media) => (
         <button
-          onClick={handleShare}
+          onClick={popupWindow.bind(this, media.url)}
           className="size-12 rounded-lg dark:bg-primary-bg bg-zinc-100 border border-zinc-200 dark:border-zinc-900 flex justify-center items-center cursor-pointer"
           key={media.name}
+          rel="noopener noreferrer"
         >
           <media.Icon size={20} />
         </button>
