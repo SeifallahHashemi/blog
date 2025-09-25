9eff7e4f7ea1198be0fc666b5f2d5d46c79bd010
S E P E H R <sepehrhashemimamaquani1996@gmail.com>
2025-09-25 07:57:22 +0330

    🧀 feat(badge): allow passing additional HTML attributes to Badge component
    - Added a generic props parameter to accept extra HTML div attributes
    - Spread the additional props onto the root div element
    - Improved flexibility for Badge component usage and customization

--- components/Pages/Badge.tsx
+++ components/Pages/Badge.tsx
@@ -4,13 +4,15 @@ import React from 'react';
 const Badge = ({
   className,
   children,
-}: Readonly<{ className?: string; children: React.ReactNode }>) => {
+  props
+}: Readonly<{ className?: string; children: React.ReactNode, props?: React.HTMLAttributes<HTMLDivElement>}>) => {
   return (
     <div
       className={cn(
         'inline-flex items-center justify-center whitespace-nowrap rounded-md transition-colors dark:bg-neutral-800 dark:border-neutral-700 gap-1',
         className
       )}
+      {...props}
     >
       {children}
     </div>
