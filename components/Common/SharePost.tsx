8d5d06df17fcc6a8e7c91dd9e23d5d836d3fc737
S E P E H R <sepehrhashemimamaquani1996@gmail.com>
2025-09-25 20:06:26 +0330

    🪒 feat(sharePost): add multiple social media share buttons
    - Remove single icon prop and implementation
    - Introduce array of social media with respective icons
    - Render buttons for Telegram, WhatsApp, Twitter, and Facebook
    - Apply consistent styling and layout for all share buttons
    -

--- components/Common/SharePost.tsx
+++ components/Common/SharePost.tsx
@@ -1,16 +1,25 @@
 'use client';
 
 import React from 'react';
-import { IconType } from 'react-icons/lib';
+import { BsWhatsapp } from 'react-icons/bs';
+import { FaFacebookSquare } from 'react-icons/fa';
+import { LiaTelegram } from 'react-icons/lia';
+import { RiTwitterXFill } from 'react-icons/ri';
 
 type SharePostProps = {
   title: string;
   url: string;
   text: string;
-  Icon: IconType;
 };
 
-const SharePost = ({ text, url, title, Icon }: SharePostProps) => {
+const socialMedia = [
+  { name: 'telegram', Icon: LiaTelegram },
+  { name: 'whatsapp', Icon: BsWhatsapp },
+  { name: 'twitter', Icon: RiTwitterXFill },
+  { name: 'facebook', Icon: FaFacebookSquare },
+];
+
+const SharePost = ({ text, url, title }: SharePostProps) => {
   const handleShare = async () => {
     if (navigator.share) {
       try {
@@ -23,9 +32,17 @@ const SharePost = ({ text, url, title, Icon }: SharePostProps) => {
     }
   };
   return (
-    <button onClick={handleShare} className="">
-      <Icon size={20} />
-    </button>
+    <div className="w-full h-full py-6 px-1 flex flex-row gap-2 flex-wrap justify-center items-center">
+      {socialMedia.map((media) => (
+        <button
+          onClick={handleShare}
+          className="size-12 rounded-lg dark:bg-primary-bg bg-zinc-100 border border-zinc-200 dark:border-zinc-900 flex justify-center items-center cursor-pointer"
+          key={media.name}
+        >
+          <media.Icon size={20} />
+        </button>
+      ))}
+    </div>
   );
 };
 
