bf28fe6155b96c62e7e2c87a3dc1c351b8519542
S E P E H R <sepehrhashemimamaquani1996@gmail.com>
2025-09-26 21:24:02 +0330

    🪾 create custom image component for sanity image type

---
+++ components/Common/SanityImage.tsx
@@ -0,0 +1,28 @@
+import { urlFor } from '@/lib/sanity.image';
+import { cn } from '@/lib/utils';
+import React from 'react';
+import Image from 'next/image';
+
+type SIProps = {
+  src: {};
+  alt: string;
+  className?: string;
+};
+
+const SanityImage = ({ src, alt, className }: SIProps) => {
+  return (
+    <Image
+      src={urlFor(src).url()}
+      alt={alt}
+      className={cn('', className)}
+      width={1920}
+      height={1080}
+      quality={100}
+      sizes='100dvw'
+      placeholder='blur'
+      blurDataURL={urlFor(src).blur(10).quality(10).url()}
+    />
+  );
+};
+
+export default SanityImage;
