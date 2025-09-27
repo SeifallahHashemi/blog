7ebe378d2e08b7c12b4b56069e246b69388d2fce
S E P E H R <sepehrhashemimamaquani1996@gmail.com>
2025-09-27 13:39:59 +0330

    🌽 feat(components): add PortableImage to CustomPortableText types
    - Import PortableImage component
    - Register PortableImage as the image type in CustomPortableText types
    - Enable

--- components/Common/CustomPortableText.tsx
+++ components/Common/CustomPortableText.tsx
@@ -1,7 +1,8 @@
 import { PortableTextComponents } from "next-sanity";
+import PortableImage from "./PortableImage";
 
 export const CustomPortableText: PortableTextComponents = {
     types: {
-        
+        image: PortableImage,
     }
 }
