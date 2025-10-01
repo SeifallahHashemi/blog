b46885cecae22dcab9407e361cec962038b44285
S E P E H R <sepehrhashemimamaquani1996@gmail.com>
2025-10-01 21:39:59 +0330

    🫛 feat(Common): add clipboard copy button to CodeBlock component
    -

--- components/Common/CodeBlock.tsx
+++ components/Common/CodeBlock.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Clipboard from './Clipboard';
 
 interface CodeBlockTypes {
     value: {
@@ -15,6 +16,7 @@ const CodeBlock = ({ value }: CodeBlockTypes) => {
     <section className=''>
         <div>
             {fileName && <p className='font-sans font-medium text-sm text-zinc-900 dark:text-zinc-200'>{fileName}</p>}
+            <Clipboard content={code} />
         </div>
     </section>
   )
