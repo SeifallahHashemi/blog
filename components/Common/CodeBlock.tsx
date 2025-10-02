bb79e2518121be8b6445c138e2758656da717961
S E P E H R <sepehrhashemimamaquani1996@gmail.com>
2025-10-02 18:32:06 +0330

    🫛 refactor(CodeBlock): remove prism-react-renderer and highlight code
    - Removed prism-react-renderer dependency and related imports
    - Deleted syntax highlighting implementation with tokens and lines
    - Simplified CodeBlock component to only render Clipboard with code content
    - Reduced

--- components/Common/CodeBlock.tsx
+++ components/Common/CodeBlock.tsx
@@ -1,7 +1,4 @@
-import React from 'react';
-import { Highlight, themes } from 'prism-react-renderer';
 import Clipboard from './Clipboard';
-import { useTheme } from 'next-themes';
 
 interface CodeBlockTypes {
   value: {
@@ -24,20 +21,6 @@ const CodeBlock = ({ value }: CodeBlockTypes) => {
         )}
         <Clipboard content={code} />
       </div>
-      <Highlight theme={themes.vsDark} language={language} code={code}>
-        {({ className, style, tokens, getLineProps, getTokenProps }) => (
-          <pre style={style} className={`${className} tracking-normal border-x border-b dark:border-zinc-800 border-zinc-200 rounded-b-lg text-sm`}>
-            {tokens.map((line, i) => (
-              <div key={i} {...getLineProps({ line })}>
-                <span className='text-sm text-zinc-500 dark:text-zinc-400 min-w-6 mr-4 inline-flex justify-center items-center select-none opacity-50 font-iranYWR bg-gray-800 border-r border-r-zinc-900'>{i + 1}</span>
-                {line.map((token, key) => (
-                  <span key={key} {...getTokenProps({ token })} className='text-sm text-zinc-500 dark:text-zinc-400 select-none opacity-50 font-mono' />
-                ))}
-              </div>
-            ))}
-          </pre>
-        )}
-      </Highlight>
     </section>
   );
 };
