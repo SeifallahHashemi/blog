2f793506af4effe7cb5d04aab850ae7fa08104db
S E P E H R <sepehrhashemimamaquani1996@gmail.com>
2025-10-02 17:18:33 +0330

    🌽 feat(components): add code block support to custom portable text
    - Import CodeBlock component into CustomPortableText
    - Register code type rendering with CodeBlock in CustomPortableText types
    - Enhance

--- components/Common/CustomPortableText.tsx
+++ components/Common/CustomPortableText.tsx
@@ -1,8 +1,10 @@
 import { PortableTextComponents } from "next-sanity";
 import PortableImage from "./PortableImage";
+import CodeBlock from "./CodeBlock";
 
 export const CustomPortableText: PortableTextComponents = {
     types: {
         image: PortableImage,
+        code: CodeBlock,
     }
 }
