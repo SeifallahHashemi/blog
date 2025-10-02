b314954e8bea2bd46e359e350dd5987ba2b05e24
S E P E H R <sepehrhashemimamaquani1996@gmail.com>
2025-10-02 23:53:35 +0330

    🫛 feat(Common): add syntax highlighting with theme support in CodeBlock
    - Integrate react-syntax-highlighter for code rendering
    - Add dark and light theme support for syntax highlighting
    - Display filename and clipboard copy button as before
    - Adjust styles for syntax highlighter container

--- components/Common/CodeBlock.tsx
+++ components/Common/CodeBlock.tsx
@@ -1,3 +1,6 @@
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import darkTheme from 'react-syntax-highlighter/dist/esm/styles/prism/atom-dark';
+import lightTheme from 'react-syntax-highlighter/dist/esm/styles/prism/duotone-light';
 import Clipboard from './Clipboard';
 
 interface CodeBlockTypes {
@@ -8,18 +11,28 @@ interface CodeBlockTypes {
   };
 }
 
-const CodeBlock = ({ value }: CodeBlockTypes) => {
-  const { code, language, filename = "Javascript" } = value;
+const CodeBlock = async ({ value }: CodeBlockTypes) => {
+  const { code, language, filename = 'Javascript' } = value;
 
   return (
-    <section className="w-full my-4" dir='ltr'>
-      <div className='flex justify-between items-center w-full bg-zinc-50 dark:bg-[#141414] border border-zinc-200 dark:border-zinc-800 rounded-t-lg px-3 py-4'>
+    <section className="w-full my-4" dir="ltr">
+      <div className="flex justify-between items-center w-full bg-zinc-50 dark:bg-[#141414] border border-zinc-200 dark:border-zinc-900 rounded-t-lg px-3 py-4">
         {filename && (
           <p className="font-sans font-medium text-sm text-zinc-900 dark:text-zinc-200">
             {filename}
           </p>
         )}
         <Clipboard content={code} />
+      </div>
+      <div className="hidden dark:inline-block w-full **:!m-0 [&_>_pre]:!rounded-t-none [&_>_pre]:!border-x [&_>_pre]:!border-b [&_>_pre]:!border-zinc-200 [&_>_pre]:dark:!border-zinc-900">
+        <SyntaxHighlighter language={language} style={darkTheme}>
+          {code}
+        </SyntaxHighlighter>
+      </div>
+      <div className="inline-block dark:hidden w-full **:!m-0 [&_>_pre]:!rounded-t-none [&_>_pre]:!border-x [&_>_pre]:!border-b [&_>_pre]:!border-zinc-200 [&_>_pre]:dark:!border-zinc-900">
+        <SyntaxHighlighter language={language} style={lightTheme}>
+          {code}
+        </SyntaxHighlighter>
       </div>
     </section>
   );
