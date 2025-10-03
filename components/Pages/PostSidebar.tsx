0c3eb83c5c2ea5a2b4f67d2e05318edcbe6fa563
S E P E H R <sepehrhashemimamaquani1996@gmail.com>
2025-10-03 16:57:25 +0330

    🍂 feat(sidebar): add tags section with badges to post sidebar
    - Introduced tags prop with type supporting icon details
    - Added UI section to display tags with custom Badge component
    - Styled tags list with flex layout and border color per tag
    - Improved sidebar division by adding dividing lines between sections
    - Wrapped author

--- components/Pages/PostSidebar.tsx
+++ components/Pages/PostSidebar.tsx
@@ -1,49 +1,92 @@
 import { cn } from '@/lib/utils';
-import { AuthorProfileType } from '@/types';
+import { AuthorProfileType, OptionalType, TagType } from '@/types';
+import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react';
+import Badge from './Badge';
+
+type TagsType = OptionalType<TagType, 'icon'> & {
+  icon: {
+    url: string;
+    alt: string;
+  };
+};
 
 type SidebarProps = {
   className?: string;
   info: AuthorProfileType;
+  tags: TagsType[];
 };
 
-const PostSidebar = ({ className, info }: SidebarProps) => {
+const PostSidebar = ({ className, info, tags }: SidebarProps) => {
   const { _id, image, name, slug, xUrl } = info;
   return (
     <div
       className={cn(
-        'w-full sticky top-0 flex flex-col gap-y-2 py-6 border-b dark:border-b-zinc-900 border-b-zinc-200',
+        'w-full sticky top-0 flex flex-col divide-y divide-zinc-200 dark:divide-zinc-900',
         className
       )}
     >
-      <div className="basic-font-styles dark:text-zinc-600 text-zinc-500 text-xs font-semibold">اطلاعات نویسنده</div>
       {/* Author section */}
-      <div className="flex items-center gap-x-2">
-        <Image
-          src={image.url}
-          alt={image.alt}
-          className="size-16 rounded-full object-cover"
-          width={100}
-          height={100}
-          placeholder={image?.url ? 'blur' : 'empty'}
-          blurDataURL={image?.lqip || ''}
-        />
-        <div className="flex flex-col gap-y-1">
-          <p className="basic-font-styles">{name}</p>
-          <Link
-            href={xUrl}
-            className="font-sans text-sky-500"
-            dir="ltr"
-            target="_blank"
-          >
-            @{slug}
-          </Link>
+      <div className="flex flex-col gap-y-2 py-6">
+        <div className="basic-font-styles dark:text-zinc-600 text-zinc-500 text-xs font-semibold">
+          اطلاعات نویسنده
+        </div>
+        <div className="flex items-center gap-x-2">
+          <Image
+            src={image.url}
+            alt={image.alt}
+            className="size-16 rounded-full object-cover"
+            width={100}
+            height={100}
+            placeholder={image?.url ? 'blur' : 'empty'}
+            blurDataURL={image?.lqip || ''}
+          />
+          <div className="flex flex-col gap-y-1">
+            <p className="basic-font-styles">{name}</p>
+            <Link
+              href={xUrl}
+              className="font-sans text-sky-500"
+              dir="ltr"
+              target="_blank"
+            >
+              @{slug}
+            </Link>
+          </div>
         </div>
       </div>
       {/* Tags section */}
-      <div></div>
+      <div className="px-1 py-6">
+        <p className="basic-font-styles dark:text-zinc-600 text-zinc-500">
+          تگ ها:{' '}
+        </p>
+        <ul className="flex flex-row flex-wrap justify-end gap-x-2">
+          {tags?.map((tag) => (
+            <li key={tag.slug}>
+              <Badge
+                className={`rounded-full w-fit h-auto py-1 px-2 border dark:border-zinc-800 border-zinc-200 overflow-clip flex flex-row gap-x-2 text-xs font-extralight leading-relaxed font-sans`}
+                props={{
+                  style: {
+                    borderColor: tag.color,
+                    borderWidth: '1px',
+                  },
+                }}
+              >
+                <Image
+                  src={tag.icon?.url || ''}
+                  alt={tag.alt || ''}
+                  width={15}
+                  height={15}
+                  className={'bg-transparent rounded-full'}
+                />
+                <span>{tag.title}</span>
+              </Badge>
+            </li>
+          ))}
+        </ul>
+      </div>
+      {/* ShareButton */}
+      <div className='px-1 py-6'></div>
     </div>
   );
 };
