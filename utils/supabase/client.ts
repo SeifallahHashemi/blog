fb677ac0a2dbb6cc03eefd263b3a9c2eb09495f5
S E P E H R <sepehrhashemimamaquani1996@gmail.com>
2025-10-12 20:02:40 +0330

    🧽 feat(header): add ProfileButton component to header
    - Import and include ProfileButton in the header's right section next to ThemeModeToggle
    - Enhance user interface with profile access button
    
    feat(supabase): add createAdminClient function for admin access
    
    - Implement new createAdminClient function using service role key
    - Enable

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -6,3 +6,10 @@ export function createClient() {
     process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
   );
 }
+
+export function createAdminClient() {
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_SRK!
+  );
+}
