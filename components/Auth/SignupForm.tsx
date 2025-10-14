dff565f473d8525f131ec3ec03eb957356135030
S E P E H R <sepehrhashemimamaquani1996@gmail.com>
2025-10-14 22:03:25 +0330

    🥞 feat(auth): integrate form validation with tanstack/react-form and zod
    - Add useForm hook with default values for email, password, and confirmPassword
    - Use signupSchema for onChange, onBlur, and onSubmit validation
    - Implement onSubmit handler to log form values
    - Prepare SignupForm component for

--- components/Auth/SignupForm.tsx
+++ components/Auth/SignupForm.tsx
@@ -2,8 +2,26 @@
 
 import React from 'react';
 import Form from 'next/form';
+import { useForm } from '@tanstack/react-form'
+import { signupSchema } from '@/utils/schema/zod-schema';
 
 const SignupForm = () => {
+  const {} = useForm({
+    defaultValues: {
+      email: '',
+      password: '',
+      confirmPassword: '',
+    },
+    validators: {
+      onChange: signupSchema,
+      onBlur: signupSchema,
+      onSubmit: signupSchema,
+    },
+    onSubmit: async (values) => {
+      console.log(values);
+      
+    }
+  })
   return <Form action="">SignupForm</Form>;
 };
 
