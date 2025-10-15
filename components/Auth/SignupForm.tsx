2b0e8fcbd39185d6207811606c23b56d2abb57a7
S E P E H R <sepehrhashemimamaquani1996@gmail.com>
2025-10-15 10:28:02 +0330

    🥞 style(auth): clean up SignupForm component formatting and imports
    - Removed unused zod import and commented type alias
    - Reformatted JSX props for better readability
    - Kept existing text and structure unchanged

--- components/Auth/SignupForm.tsx
+++ components/Auth/SignupForm.tsx
@@ -4,10 +4,7 @@ import { signupSchema } from '@/utils/schema/zod-schema';
 import { useForm } from '@tanstack/react-form';
 import React from 'react';
 import Form from 'next/form';
-import * as z from 'zod';
 import AuthFormContainer from './AuthFormContainer';
-
-// type FormData = z.infer<typeof signupSchema>
 
 const SignupForm = () => {
   const {} = useForm({
@@ -26,7 +23,11 @@ const SignupForm = () => {
     },
   });
   return (
-    <AuthFormContainer title='ثبت نام' description='لطفا اطلاعات خود را به دقت وارد کنید' footer={<div>footer</div>}>
+    <AuthFormContainer
+      title="ثبت نام"
+      description="لطفا اطلاعات خود را به دقت وارد کنید"
+      footer={<div>footer</div>}
+    >
       <Form action="">SignupForm</Form>
     </AuthFormContainer>
   );
