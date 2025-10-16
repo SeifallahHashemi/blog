4e5c25926a2f9d9bdb993a1ee8819905e5375079
S E P E H R <sepehrhashemimamaquani1996@gmail.com>
2025-10-16 10:18:59 +0330

    🦟 feat(auth): add email verification form to verify page
    - Replace placeholder div with AuthFormContainer component
    - Add title and description for email verification step
    - Embed VerifyForm component inside AuthFormContainer
    - Improve user interface for email code input verification

--- app/auth/verify/page.tsx
+++ app/auth/verify/page.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import AuthFormContainer from '@/components/Auth/AuthFormContainer';
+import VerifyForm from '@/components/Auth/VerifyForm';
+import React from 'react';
 
 const VerifyPage = () => {
   return (
-    <div>VerifyPage</div>
-  )
-}
+    <AuthFormContainer
+      title="تاییدیه ایمیل"
+      description="لطفا کد تاییدیه که به ایمیل شما فرستادیم را وارد کنید"
+    >
+      <VerifyForm />
+    </AuthFormContainer>
+  );
+};
 
-export default VerifyPage
+export default VerifyPage;
