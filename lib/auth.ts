89a1e7d7db655f78f4527d6f604b31a48073960e
S E P E H R <sepehrhashemimamaquani1996@gmail.com>
2025-10-16 17:43:52 +0330

    🦜 feat(auth): add login function and refactor verifyOtp function
    - Refactor verifyOtp function for improved formatting and consistency
    - Add new login function to handle user sign-in with email and password
    - Use createClient for Supabase client initialization in both functions
    - Implement error handling by throwing errors in both functions
    - Return data from Supabase authentication responses in both functions

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,19 +1,40 @@
-import { createClient } from "@/utils/supabase/client";
+import { createClient } from '@/utils/supabase/client';
 
-export async function verifyOtp({ email, otp }: { email: string; otp: string }) {
-    const supabase = createClient();
+export async function verifyOtp({
+  email,
+  otp,
+}: {
+  email: string;
+  otp: string;
+}) {
+  const supabase = createClient();
 
-    const isPasswordReset = sessionStorage.getItem('isPasswordReset') == 'true';
+  const isPasswordReset = sessionStorage.getItem('isPasswordReset') == 'true';
 
-    const { data, error } = await supabase.auth.verifyOtp({
-        email,
-        token: otp,
-        type: isPasswordReset ? 'recovery' : 'email',
-    });
+  const { data, error } = await supabase.auth.verifyOtp({
+    email,
+    token: otp,
+    type: isPasswordReset ? 'recovery' : 'email',
+  });
 
-    if (error) {
-        throw error
-    }
+  if (error) {
+    throw error;
+  }
 
-    return data;
+  return data;
+}
+
+export async function login(email: string, password: string) {
+  const supabase = createClient();
+
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+
+  if (error) {
+    throw error;
+  }
+
+  return data;
 }
