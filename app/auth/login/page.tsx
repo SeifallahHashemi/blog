9b9105c73eaa6df9b5a9c26fc76f8dc0e3de36fd
S E P E H R <sepehrhashemimamaquani1996@gmail.com>
2025-10-16 17:37:54 +0330

    🧈 feat(auth): add signup link to login page footer
    - Add footer section to login form container
    - Include prompt for users without an account
    - Add clickable link to registration page with styling
    - Enhance navigation experience on login page

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,11 +1,23 @@
 import AuthFormContainer from '@/components/Auth/AuthFormContainer';
 import LoginForm from '@/components/Auth/LoginForm';
+import Link from 'next/link';
 
 export default function LoginPage() {
   return (
     <AuthFormContainer
       title="ورود"
       description="برای دسترسی به حساب کاربری خود، اطلاعات کاربری خود را وارد کنید"
+      footer={
+        <div className="w-full inline-flex justify-center items-center gap-1.5 text-center text-xs font-normal">
+          اشتراک ندارید؟{' '}
+          <Link
+            href="/auth/signup"
+            className="text-blue-600 hover:underline text-xs font-normal"
+          >
+            ثبت نام کنید
+          </Link>
+        </div>
+      }
     >
       <LoginForm />
     </AuthFormContainer>
