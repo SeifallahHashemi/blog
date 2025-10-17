09e0849e8c795ae7994fd2793158de14a9407fa0
S E P E H R <sepehrhashemimamaquani1996@gmail.com>
2025-10-17 22:26:11 +0330

    🐄 refactor(NewPasswordForm): remove inline FieldInfo component and import from separate file

--- components/Auth/NewPasswordForm.tsx
+++ components/Auth/NewPasswordForm.tsx
@@ -4,7 +4,7 @@ import { updatePassword } from '@/lib/auth';
 import { translateSupabaseError } from '@/utils/errors/supabase-error';
 import { newPasswordSchema } from '@/utils/schema/zod-schema';
 import { createClient } from '@/utils/supabase/client';
-import { AnyFieldApi, useForm } from '@tanstack/react-form';
+import { useForm } from '@tanstack/react-form';
 import React, { useEffect, useState } from 'react';
 import { EyeIcon, EyeOffIcon } from 'lucide-react';
 import { useRouter } from 'next/navigation';
@@ -14,6 +14,7 @@ import * as z from 'zod';
 import { Button } from '../ui/button';
 import { Input } from '../ui/input';
 import { Label } from '../ui/label';
+import { FieldInfo } from './FieldInfo';
 
 type formDate = z.infer<typeof newPasswordSchema>;
 
@@ -200,23 +201,5 @@ const NewPasswordForm = () => {
     </form>
   );
 };
-
-function FieldInfo({ field }: { field: AnyFieldApi }) {
-  return (
-    <div className="flex flex-row gap-2 flex-wrap my-1">
-      {field.state.meta.isTouched && !field.state.meta.isValid
-        ? field.state.meta.errors.map((err, ind) => (
-            <em
-              key={ind}
-              className="text-xs leading-relaxed tracking-tight text-red-400 font-normal"
-            >
-              {ind + 1}- {err.message}
-            </em>
-          ))
-        : null}
-      {field.state.meta.isValidating ? 'درحال اعتبارسنجی ...' : null}
-    </div>
-  );
-}
 
 export default NewPasswordForm;
