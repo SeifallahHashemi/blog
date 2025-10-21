ed1a26d4d9a83a5b254605e1204a471186f8b6fb
S E P E H R <sepehrhashemimamaquani1996@gmail.com>
2025-10-21 17:38:02 +0330

    🍆 style(middleware): format code and update middleware config paths
    - Added missing semicolons for consistent code formatting
    - Included additional path patterns for /auth and /dashboard in middleware config
    - Improved readability by adjusting braces and indentation

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,8 @@
-import { type NextRequest } from 'next/server'
-import { updateSession } from './utils/supabase/middleware'
+import { type NextRequest } from 'next/server';
+import { updateSession } from './utils/supabase/middleware';
 
 export async function middleware(request: NextRequest) {
-  return await updateSession(request)
+  return await updateSession(request);
 }
 
 export const config = {
@@ -15,5 +15,7 @@ export const config = {
      * Feel free to modify this pattern to include more paths.
      */
     '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    '/auth/:path*',
+    '/dashboard/:path*',
   ],
-}
+};
