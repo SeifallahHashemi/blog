5d66a344a02236db3392cbe723f789b3a1ffc29f
S E P E H R <sepehrhashemimamaquani1996@gmail.com>
2025-10-21 17:46:25 +0330

    🍲 refactor(theme): improve theme state handling and cleanup
    - Replace isMounted state with useRef for better performance
    - Simplify try-catch block in getSavedThemeColor function
    - Add cleanup function to reset isMounted ref on unmount
    - Adjust import statements for consistency and style
    - Format code according to project style guidelines

--- context/ThemeDataProvider.tsx
+++ context/ThemeDataProvider.tsx
@@ -1,41 +1,35 @@
-"use client";
-import setGlobalColorTheme from "@/lib/theme-colors";
-import {ThemeProviderProps, useTheme} from "next-themes";
-import React, {
-  createContext,
-  useContext,
-  useEffect,
-  useState,
-} from "react";
+'use client';
+
+import setGlobalColorTheme from '@/lib/theme-colors';
+import { createContext, useContext, useEffect, useRef, useState } from 'react';
+import { useTheme, type ThemeProviderProps } from 'next-themes';
 
 const ThemeContext = createContext<ThemeColorStateParams>(
-  {} as ThemeColorStateParams,
+  {} as ThemeColorStateParams
 );
 
-export default function ThemeDataProvider({
-  children,
-}: ThemeProviderProps) {
+export default function ThemeDataProvider({ children }: ThemeProviderProps) {
   const getSavedThemeColor = () => {
     try {
-      return (localStorage.getItem("themeColor") as ThemeColors) || "Zinc";
-    } catch {
-      console.log("theme error")
-    }
+      return (localStorage.getItem('themeColor') as ThemeColors) || 'Zinc';
+    } catch {}
   };
 
   const [themeColor, setThemeColor] = useState<ThemeColors>(
-    getSavedThemeColor() as ThemeColors,
+    getSavedThemeColor() as ThemeColors
   );
-  const [isMounted, setIsMounted] = useState(false);
+  const isMounted = useRef<boolean>(false);
   const { theme } = useTheme();
 
   useEffect(() => {
-    localStorage.setItem("themeColor", themeColor);
-    setGlobalColorTheme(theme as "light" | "dark", themeColor);
+    localStorage.setItem('themeColor', themeColor);
+    setGlobalColorTheme(theme as 'light' | 'dark', themeColor);
 
-    if (!isMounted) {
-      setIsMounted(true);
-    }
+    isMounted.current = true;
+
+    return () => {
+      isMounted.current = false;
+    };
   }, [themeColor, theme, isMounted]);
 
   if (!isMounted) {
