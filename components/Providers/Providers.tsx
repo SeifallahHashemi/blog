66a1812944976a7ce6a73f876137cdf5a33cb6a8
S E P E H R <sepehrhashemimamaquani1996@gmail.com>
2025-10-26 18:25:37 +0330

    🥔 feat(providers): integrate QueryClientProvider with getClientQuery for improved data management

--- components/Providers/Providers.tsx
+++ components/Providers/Providers.tsx
@@ -2,20 +2,27 @@
 
 import MotionLenis from '@/components/Providers/MotionLenis';
 import ThemeDataProvider from '@/context/ThemeDataProvider';
+import { getClientQuery } from '@/lib/get-client-query';
 import { TanStackDevtools } from '@tanstack/react-devtools';
 import { FormDevtoolsPlugin } from '@tanstack/react-form-devtools';
+import { QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import { ThemeProvider } from 'next-themes';
 import { NuqsAdapter } from 'nuqs/adapters/next/app';
 import { Toaster } from 'sonner';
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
+  const queryClient = getClientQuery();
   return (
     <>
       <ThemeProvider enableSystem attribute={'class'} defaultTheme={'system'}>
         <ThemeDataProvider>
           <MotionLenis>
-            <NuqsAdapter>{children}</NuqsAdapter>
+            <NuqsAdapter>
+              <QueryClientProvider client={queryClient}>
+                {children}
+              </QueryClientProvider>
+            </NuqsAdapter>
           </MotionLenis>
         </ThemeDataProvider>
       </ThemeProvider>
