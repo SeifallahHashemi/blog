4465b4ef16751fac32597362c8b7a76dfaf5ad8a
S E P E H R <sepehrhashemimamaquani1996@gmail.com>
2025-11-14 18:11:18 +0330

    🐃 remove supabase

--- supabase/migrations/20251113155055_add-like-dislike-count-column.sql
+++
@@ -1,5 +0,0 @@
-alter table "public"."comments" add column "dislike_count" integer default 0;
-
-alter table "public"."comments" add column "like_count" integer default 0;
-
-
--- supabase/migrations/20251113162141_remote_schema.sql
+++
@@ -1,5 +0,0 @@
-alter table "public"."comments" drop column "dislike_count";
-
-alter table "public"."comments" drop column "like_count";
-
-
