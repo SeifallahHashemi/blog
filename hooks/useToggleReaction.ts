d8f0cfc7ae0c2463df40929f6e5ebd46074f3759
S E P E H R <sepehrhashemimamaquani1996@gmail.com>
2025-11-24 10:18:35 +0330

    🫑 feat(reactions): integrate addReactionMutationOptions in useToggleReaction hook

--- hooks/useToggleReaction.ts
+++ hooks/useToggleReaction.ts
@@ -1,9 +1,10 @@
 'use client';
 
 import { getClientQuery } from '@/lib/get-client-query';
+import { addReactionMutationOptions } from '@/utils/supabase/user';
 import { useMutation } from '@tanstack/react-query';
 
 const useToggleReaction = (postId: string) => {
   const qc = getClientQuery();
-  return useMutation();
+  return useMutation(addReactionMutationOptions(qc, postId, postId));
 };
