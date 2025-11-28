844a451790350c4b3d2d57615d423e1d923f0c85
S E P E H R <sepehrhashemimamaquani1996@gmail.com>
2025-11-28 17:24:37 +0330

    🐓 feat(providers): add initial UserProvider component
    - Create UserProvider functional component returning an empty div
    - Add placeholder comment for future children prop typings
    - Set component as client-side only with 'use client' directive
    - Prepare UserProvider for further development and integration

---
+++ components/Providers/UserProvider.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import React from 'react';
+
+const UserProvider = () => {
+  return <div />;
+};
+
+export default UserProvider;
+
+/*
+* {
+  children,
+}: Readonly<{ children: React.ReactNode }>
+* */
