6d78e00f6d73f97921741b23fb2a9573c211f866
S E P E H R <sepehrhashemimamaquani1996@gmail.com>
2025-11-28 17:24:15 +0330

    🍡 feat(layout): add user data prefetch and hydration in dashboard layout
    - Introduce React Query client prefetching for user and user profile data
    - Wrap layout with HydrationBoundary to enable React Query state hydration
    - Import related user query utilities and React Query functions
    - Prepare DashboardLayout as an async component for data fetching
    - Comment out UserProvider usage for potential future inclusion

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,18 +1,32 @@
 import DockAnimation from '@/components/Animation/DockAnimation';
 import Footer from '@/components/Global/Footer';
 import Header from '@/components/Global/Header';
+import UserProvider from '@/components/Providers/UserProvider';
+import { getClientQuery } from '@/lib/get-client-query';
+import { getUser } from '@/utils/supabase/queries';
+import { userOptions, userProfileOptions } from '@/utils/supabase/user';
+import { HydrationBoundary, dehydrate } from '@tanstack/react-query';
 import React from 'react';
 
-const DashboardLayout = ({
+const DashboardLayout = async ({
   children,
 }: Readonly<{ children: React.ReactNode }>) => {
+  const queryClient = getClientQuery();
+
+  await queryClient.prefetchQuery(userOptions);
+
+  const user = await getUser();
+
+  await queryClient.prefetchQuery(userProfileOptions(user.id));
+
   return (
-    <>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      {/*<UserProvider />*/}
       <Header />
       <DockAnimation />
       {children}
       <Footer />
-    </>
+    </HydrationBoundary>
   );
 };
 
