832f5adb5421916508010392a6b605585ac01d05
S E P E H R <sepehrhashemimamaquani1996@gmail.com>
2025-11-28 20:07:24 +0330

    🐓 feat(UserProvider): implement user information synchronization in UserProvider component

--- components/Providers/UserProvider.tsx
+++ components/Providers/UserProvider.tsx
@@ -1,21 +1,32 @@
 'use client';
 
-import React from 'react';
+import { useUserStore } from '@/store/userStore';
+import { useEffect } from 'react';
 
 interface User {
   id: string;
-  userName: string;
+  name: string;
   fullName: string;
 }
 
-const UserProvider = ({
-  children,
-  user,
-}: {
-  children: React.ReactNode;
-  user: User;
-}) => {
-  return children;
+const UserProvider = ({ user }: { user: User }) => {
+  const updateUserInformation = useUserStore(
+    (state) => state.updateUserInformation
+  );
+  const storedUser = useUserStore((state) => state.user);
+
+  useEffect(() => {
+    if (
+      !storedUser ||
+      storedUser.id !== user.id ||
+      storedUser.name !== user.name ||
+      storedUser.fullName !== user.fullName
+    ) {
+      updateUserInformation(user);
+    }
+  }, [user, storedUser, updateUserInformation]);
+
+  return null;
 };
 
 export default UserProvider;
