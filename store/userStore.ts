88efa844df723e5cf507e28a4ed17f845f1d5b23
S E P E H R <sepehrhashemimamaquani1996@gmail.com>
2025-11-28 20:09:17 +0330

    🥑 refactor(userStore): simplify user state management and add reset functionality

--- store/userStore.ts
+++ store/userStore.ts
@@ -1,52 +1,33 @@
 import { create } from 'zustand';
 import { createJSONStorage, devtools, persist } from 'zustand/middleware';
 
-type UserUpdateValue = {
+type User = {
   id: string;
   name: string;
   fullName: string;
 };
-type UserState = {
-  userId: string;
-  userName: string;
-  userFullName: string;
-};
 
 type UserStore = {
-  user: UserState;
-  updateUserInformation: (value: UserUpdateValue) => void;
+  user: User | null;
+  updateUserInformation: (value: User) => void;
+  resetUser: () => void;
 };
 
 export const useUserStore = create<UserStore>()(
   devtools(
     persist(
       (set) => ({
-        user: {
-          userId: '',
-          userName: '',
-          userFullName: '',
-        },
-        updateUserInformation: (value: UserUpdateValue) => {
-          set(
-            {
-              user: {
-                userId: value.id,
-                userName: value.name,
-                userFullName: value.fullName,
-              },
-            },
-            false,
-            'user/updateInformation'
-          );
-        },
+        user: null,
+
+        updateUserInformation: (value: User) =>
+          set({ user: value }, false, 'user/updateInformation'),
+
+        resetUser: () => set({ user: null }, false, 'user/reset'),
       }),
       {
         name: 'user-information',
         storage: createJSONStorage(() => localStorage),
       }
-    ),
-    {
-      name: 'User Store Persisted',
-    }
+    )
   )
 );
