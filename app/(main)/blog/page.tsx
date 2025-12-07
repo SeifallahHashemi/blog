e7dc223225b4411e97a08db78dbd5871a75c9013
S E P E H R <sepehrhashemimamaquani1996@gmail.com>
2025-12-07 16:19:24 +0330

    🎳 refactor(blog): replace AllPosts with PostsList component
    - Removed manual mapping of posts inside BlogPage component
    - Replaced with PostsList component to handle post list rendering
    - Simplified BlogPage JSX for better readability and maintenance
    - Updated import to reflect new PostsList component usage

--- app/(main)/blog/page.tsx
+++ app/(main)/blog/page.tsx
@@ -1,4 +1,4 @@
-import AllPosts from '@/components/Pages/AllPosts';
+import PostsList from '@/components/Pages/PostsList';
 import { sanityFetch } from '@/lib/sanity.client';
 import { allPostsQuery } from '@/lib/sanity.query';
 import { PostsType } from '@/types';
@@ -19,25 +19,7 @@ const BlogPage = async () => {
         آخرین اخبار و مقاله های برنامه نویسی
       </h1>
       <ul className="mt-4 flex flex-row gap-4">
-        {posts.map((post: PostsType) => (
-          <li
-            key={post._id}
-            className={
-              'w-md overflow-clip lg:max-w-[400px] bg-primary-bg py-3 rounded-lg'
-            }
-          >
-            <AllPosts
-              _createdAt={post._createdAt}
-              author={post.author}
-              coverImage={post.coverImage}
-              date={post.date}
-              isPublished={post.isPublished}
-              slug={post.slug}
-              tags={post.tags}
-              title={post.title}
-            />
-          </li>
-        ))}
+        <PostsList posts={posts} />
       </ul>
     </section>
   );
