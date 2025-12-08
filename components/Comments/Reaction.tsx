e405a1c19a606b7f78254abf6b2b040649b84d38
S E P E H R <sepehrhashemimamaquani1996@gmail.com>
2025-12-08 14:43:48 +0330

    🎂 refactor(comments): replace reaction spans with button group for interaction
    - Replaced like and dislike reaction display spans with button components
    - Added clickable buttons to handle like and dislike reactions
    - Integrated useToggleReaction hook's mutate function for reaction updates
    - Styled buttons with outline variant and cursor pointer for better UX
    - Grouped buttons using ButtonGroup component for consistent layout

--- components/Comments/Reaction.tsx
+++ components/Comments/Reaction.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { Separator } from '@/components/ui/separator';
+import { Button } from '@/components/ui/button';
+import { ButtonGroup } from '@/components/ui/button-group';
 import useToggleReaction from '@/hooks/useToggleReaction';
 import React from 'react';
 import { SlDislike, SlLike } from 'react-icons/sl';
@@ -16,29 +17,43 @@ const Reaction = ({
   postId: string;
   commentId: string;
 }) => {
-  console.log('postId in reaction:', postId);
-  console.log('commentId in reaction:', commentId);
-  // const {} = useToggleReaction(postId, commentId);
+  const { mutate } = useToggleReaction(postId, commentId);
+
+  const reactionHandler = (reaction: 'like' | 'dislike') => {
+    mutate({ commentId, reaction });
+  };
+
   return (
-    <>
-      <span
-        aria-label="Likes"
-        className={
-          'flex flex-row flex-nowrap gap-x-2 justify-center items-center cursor-pointer'
-        }
+    <ButtonGroup className={'font-iranYWL font-medium text-sm'}>
+      <Button
+        variant={'outline'}
+        className={'cursor-pointer'}
+        onClick={() => reactionHandler('like')}
       >
-        <SlLike /> {like_count}
-      </span>
-      <Separator orientation={'vertical'} className={'!h-5'} />
-      <span
-        aria-label="Dislikes"
-        className={
-          'flex flex-row-reverse flex-nowrap gap-x-2 justify-center items-center cursor-pointer'
-        }
+        <span
+          aria-label="Likes"
+          className={
+            'flex flex-row flex-nowrap gap-x-2 justify-center items-center'
+          }
+        >
+          <SlLike /> {like_count}
+        </span>
+      </Button>
+      <Button
+        variant={'outline'}
+        className={'cursor-pointer'}
+        onClick={() => reactionHandler('dislike')}
       >
-        <SlDislike /> {dislike_count}
-      </span>
-    </>
+        <span
+          aria-label="Dislikes"
+          className={
+            'flex flex-row-reverse flex-nowrap gap-x-2 justify-center items-center'
+          }
+        >
+          <SlDislike /> {dislike_count}
+        </span>
+      </Button>
+    </ButtonGroup>
   );
 };
 
